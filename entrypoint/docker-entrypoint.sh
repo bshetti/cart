85100c038250a60c83706aa7d98c5942ecb3e97c
bshetti <billshetti@gmail.com>
2019-01-30 07:25:13 +0000

    modified for docker compose

--- entrypoint/docker-entrypoint.sh
+++ entrypoint/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until /usr/bin/redis-cli -h cartredis ping | grep -q 'PONG'; do
+until /usr/bin/redis-cli -h reddb ping | grep -q 'PONG'; do
   >&2 echo "redis is unavailable - sleeping" then
   sleep 1
 done
