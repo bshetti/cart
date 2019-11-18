cfff08da019a15a5ed0fa8f7621bb463effc8c33
Shrivatsa Upadhye <ushrivatsa@gmail.com>
2019-11-18 15:50:13 -0800

    Update docker-entrypoint.sh

--- entrypoint/docker-entrypoint.sh
+++ entrypoint/docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-until /usr/bin/redis-cli -h "${REDIS_HOST}" -a "${REDIS_PASSWORD:-}" ping | grep -q 'PONG'; do
+until /usr/bin/redis-cli -h $REDIS_HOST -p $REDIS_PORT ping | grep -q 'PONG'; do
   >&2 echo "redis is unavailable - sleeping" then
   sleep 1
 done
-
 echo "Apply flask now"
 python3 cart.py
 exec "$@"
